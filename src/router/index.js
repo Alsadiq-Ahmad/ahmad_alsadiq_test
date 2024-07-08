82de2fbd1873120a9d5a66ceeb0fad9e287726d2
Ahmad Alsadiq <alsadiqahmad1@gmail.com>
2024-07-08 13:54:52 +0300

    second commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginPage from '@/views/LoginPage.vue';
 import NotificationSettingPage from '@/views/NotificationSettingPage.vue';
+import NotFound from '@/views/NotFound.vue';
 
 const routes = [
   {
@@ -12,7 +13,12 @@ const routes = [
     path: '/setting',
     name: 'Setting',
     component: NotificationSettingPage,
-  }
+  },
+  {
+    path: '/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  },
 ];
 
 const router = createRouter({
